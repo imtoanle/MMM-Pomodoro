f8a1d4689159cb848d7dc4c70a785d9f529aafbb
Toan Le <ktoanlba@gmail.com>
2021-10-06 11:33:51 +0700

    update

--- MMM-Pomodoro.js
+++ MMM-Pomodoro.js
@@ -1,176 +1,237 @@
 /* Some code snippets of this file were copied from the default alert module https://github.com/MichMich/MagicMirror/tree/development/modules/default/alert */
 
+var self;
+
 Module.register("MMM-Pomodoro", {
-defaults: {
-	animation: true
-},
+	defaults: {
+		animation: true
+	},
 
-getStyles: function() {
-	return ["MMM-Pomodoro.css"];
-},
+	getStyles: function() {
+		return ["MMM-Pomodoro.css"];
+	},
 
-start: function() {
-	this.isVisible = false;
-	this.firstMessage = true;
-},
+	start: function() {
+		self = this;
+		this.isVisible = false;
+		this.firstMessage = true;
+	},
 
-notificationReceived: function(notification, payload, sender) {
-  switch(notification) {
-    case "START_TIMER":
-		this.minutes = Math.floor(payload / 60);
-		this.seconds = (payload % 60);
-		this.initialisePomodoro(true);
-		break
-	case "INTERRUPT_POMODORO":
+	notificationReceived: function(notification, payload, sender) {
+		switch(notification) {
+			case "START_TIMER":
+			this.startTimer(payload.seconds, payload.type);
+			break
+		case "INTERRUPT_POMODORO":
+			this.stopTimer();
+			break
+		case "PAUSE_POMODORO":
+			clearInterval(this.pomodoro);
+			break
+		case "UNPAUSE_TIMER":
+			if(this.minutes > -1 && this.seconds > -1) {
+				this.initialisePomodoro(true);
+			}
+			break
+		case "START_STOPWATCH":
+			this.minutes = 0;
+			this.seconds = 0;
+			this.initialisePomodoro(false);
+			break
+		case "UNPAUSE_STOPWATCH":
+			if(this.minutes > -1 && this.seconds > -1) {
+				this.initialisePomodoro(false);
+			}
+		}
+	},
+
+	// socketNotificationReceived from helper
+	socketNotificationReceived: function (notification, payload) {
+		if(notification === "MMM-Pomodoro-SAVEDATA") {
+			
+			switch(payload.next_type) {
+				case "relax-30":
+					this.displayMessageNoPopup("Do you want to long relax (30 mins) ?");
+					break
+				case "relax-5":
+					this.displayMessageNoPopup("Do you want to relax 5 mins ?");
+					break
+				case "pomodoro":
+					this.displayMessageNoPopup("Do you want to start another pomodoro ?");
+			}
+
+			this.displayMessageNoPopup('Yes', 'width-20', true, this.agreeClicked(payload.next_type));
+			this.displayMessageNoPopup('No', 'width-20', true, this.disagreeClicked);
+		}
+	},
+
+	startTimer: function(seconds, type){
+		this.minutes = Math.floor(seconds / 60);
+		this.seconds = (seconds % 60);
+		this.initialisePomodoro(true, type);
+	},
+	stopTimer: function(){
 		this.minutes = -1;
 		this.seconds = -1;
 		clearInterval(this.pomodoro);
 		this.removeOverlay();
-	  	break
-	case "PAUSE_POMODORO":
-		clearInterval(this.pomodoro);
-		break
-	case "UNPAUSE_TIMER":
-		if(this.minutes > -1 && this.seconds > -1) {
-			this.initialisePomodoro(true);
-		}
-		break
-	case "START_STOPWATCH":
-		this.minutes = 0;
-		this.seconds = 0;
-		this.initialisePomodoro(false);
-		break
-	case "UNPAUSE_STOPWATCH":
-		if(this.minutes > -1 && this.seconds > -1) {
-			this.initialisePomodoro(false);
-		}
-  }
-},
+	},
 
-initialisePomodoro: function(isCounter){
-	clearInterval(this.pomodoro);
-	if(this.isVisible) {
-    	this.removeOverlay();
-    }
-    this.createOverlay();
-    this.pomodoro = setInterval(()=>{
-	if(isCounter) {
-	  	this.createTimer()
-	} else {
-		this.createStopwatch()
-	}
-  	}, 1000)
-},
-
-createOverlay: function() {
-	const overlay = document.createElement("div");
-	overlay.id = "overlay";
-	overlay.innerHTML += '<div class="black_overlay"></div>';
-	document.body.insertBefore(overlay, document.body.firstChild);
-	this.ntf = document.createElement("div")
-	this.isVisible = true;
-},
-
-removeOverlay: function() {
-	const overlay = document.getElementById("overlay");
-	overlay.parentNode.removeChild(overlay);
-	document.body.removeChild(this.ntf);
-	this.isVisible = false;
-	this.firstMessage = true;
-},
-
-displayMessagePopup: function(message) {
-  let strinner = '<div class="ns-box-inner">';
-  strinner += "<span class='regular normal medium'>" + message + "</span>";
-  strinner += "</div>";
-  this.ntf.innerHTML = strinner;
-  this.ntf.className = "ns-alert ns-growl ns-effect-jelly ns-type-notice ns-show"
-  document.body.insertBefore(this.ntf, document.body.nextSibling);
-},
-
-displayMessageNoPopup: function(message) {
-  let strinner = '<div class="ns-box-inner">';
-  strinner += "<span class='regular normal medium'>" + message + "</span>";
-  strinner += "</div>";
-  this.ntf.innerHTML = strinner;
-  if(this.firstMessage) {
-  	this.ntf.className = "ns-alert ns-growl ns-effect-jelly ns-type-notice ns-show"
-  	document.body.insertBefore(this.ntf, document.body.nextSibling);
-  	this.firstMessage = false;
-  }
-},
-
-endingSound: function(soundName) {
-	let sound = new Audio();
-	sound.src = `modules/MMM-Pomodoro/sounds/${soundName}.wav`;
-	sound.play();
-},
-
-createTimer: function() {
-		if(this.minutes == 0 && this.seconds == 0){
-			this.decreaseTime();
-			this.displayMessageNoPopup('Done');
-			this.endingSound("pomodoro");
-			// this.sendSocketNotification("MMM-Pomodoro-SAVEDATA", { time: new Date });
-			setTimeout(() => {
-				this.removeOverlay()
-			}, 3000);
-		}
-		if(this.minutes > 0 || this.seconds > 0) {
-			if(this.config.animation) {
-				if(this.seconds < 10) {
-					this.displayMessagePopup(this.minutes + ':0' + this.seconds);
-				} else {
-					this.displayMessagePopup(this.minutes + ':' + this.seconds);
-				}
-			} else {	
-				if(this.seconds < 10) {
-					this.displayMessageNoPopup(this.minutes + ':0' + this.seconds);
-				} else {
-					this.displayMessageNoPopup(this.minutes + ':' + this.seconds);
-				}
+	agreeClicked: function(type) {
+		return function() {
+			switch(type) {
+				case "relax-30":
+					self.startTimer(30, type);
+					break
+				case "relax-5":
+					self.startTimer(5, type);
+					break
+				case "pomodoro":
+					self.startTimer(25, type);
 			}
-			this.decreaseTime();
-		}
-},
+		};
+	},
 
-createStopwatch: function() {
-	if(this.config.animation) {
-		if(this.seconds < 10) {
-			this.displayMessagePopup(this.minutes + ':0' + this.seconds);
+	disagreeClicked: function() {
+		self.stopTimer();
+	},
+
+	initialisePomodoro: function(isCounter, pomodoroType){
+		pomodoroType = typeof pomodoroType !== 'undefined' ? pomodoroType : "pomodoro";
+
+		clearInterval(this.pomodoro);
+		if(this.isVisible) {
+				this.removeOverlay();
+			}
+			this.createOverlay();
+			this.pomodoro = setInterval(()=>{
+		if(isCounter) {
+				this.createTimer(pomodoroType)
 		} else {
-			this.displayMessagePopup(this.minutes + ':' + this.seconds);
+			this.createStopwatch()
 		}
-	} else {
+			}, 1000)
+	},
+
+	createOverlay: function() {
+		const overlay = document.createElement("div");
+		overlay.id = "overlay";
+		overlay.innerHTML += '<div class="black_overlay"></div>';
+		document.body.insertBefore(overlay, document.body.firstChild);
+		
+		this.ntf = document.createElement("div")
+		this.isVisible = true;
+	},
+
+	removeOverlay: function() {
+		const overlay = document.getElementById("overlay");
+		overlay.parentNode.removeChild(overlay);
+		document.body.removeChild(this.ntf);
+		this.isVisible = false;
+		this.firstMessage = true;
+	},
+
+	displayMessagePopup: function(message) {
+		let strinner = '<div class="ns-box-inner">';
+		strinner += "<span class='regular normal medium'>" + message + "</span>";
+		strinner += "</div>";
+		this.ntf.innerHTML = strinner;
+		this.ntf.className = "ns-alert ns-growl ns-effect-jelly ns-type-notice ns-show"
+		document.body.insertBefore(this.ntf, document.body.nextSibling);
+	},
+
+	displayMessageNoPopup: function(message, additionClasses, isAppended, callback) {
+		callback = typeof callback !== 'undefined' ? callback : function(){};
+		additionClasses = typeof additionClasses !== 'undefined' ? additionClasses : "";
+		isAppended = typeof isAppended !== 'undefined' ? isAppended : false;
+
+		let child = document.createElement("div");
+		child.className = `${additionClasses} alert-box ns-alert ns-growl ns-effect-jelly ns-type-notice ns-show`;
+		child.innerHTML = `<div class="ns-box-inner"><span class='regular normal medium'>${message}</span></div>`;
+		child.onclick = callback;
+		if (isAppended) {
+			this.ntf.appendChild(child);
+		} else {
+			this.ntf.innerHTML = child.outerHTML;
+		}
+
+		if(this.firstMessage) {
+			this.ntf.className = "pomodoro-section"
+			document.body.insertBefore(this.ntf, document.body.nextSibling);
+			this.firstMessage = false;
+		}
+	},
+
+	endingSound: function(soundName) {
+		let sound = new Audio();
+		sound.src = `modules/MMM-Pomodoro/sounds/${soundName}.wav`;
+		sound.play();
+	},
+
+	createTimer: function(pomodoroType) {
+			if(this.minutes == 0 && this.seconds == 0){
+				this.decreaseTime();
+				this.endingSound("pomodoro");
+				this.sendSocketNotification("MMM-Pomodoro-SAVEDATA", { type: pomodoroType, time: new Date });
+				// setTimeout(() => {
+				// 	this.removeOverlay()
+				// }, 3000);
+			}
+			if(this.minutes > 0 || this.seconds > 0) {
+				if(this.config.animation) {
+					if(this.seconds < 10) {
+						this.displayMessagePopup(this.minutes + ':0' + this.seconds);
+					} else {
+						this.displayMessagePopup(this.minutes + ':' + this.seconds);
+					}
+				} else {	
+					if(this.seconds < 10) {
+						this.displayMessageNoPopup(this.minutes + ':0' + this.seconds, "watch-number");
+					} else {
+						this.displayMessageNoPopup(this.minutes + ':' + this.seconds, "watch-number");
+					}
+				}
+				this.decreaseTime();
+			}
+	},
+
+	createStopwatch: function() {
+		if(this.config.animation) {
 			if(this.seconds < 10) {
-			this.displayMessageNoPopup(this.minutes + ':0' + this.seconds);
+				this.displayMessagePopup(this.minutes + ':0' + this.seconds);
+			} else {
+				this.displayMessagePopup(this.minutes + ':' + this.seconds);
+			}
 		} else {
-			this.displayMessageNoPopup(this.minutes + ':' + this.seconds);
+				if(this.seconds < 10) {
+				this.displayMessageNoPopup(this.minutes + ':0' + this.seconds);
+			} else {
+				this.displayMessageNoPopup(this.minutes + ':' + this.seconds);
+			}
 		}
-	}
-	this.increaseTime();
-},
+		this.increaseTime();
+	},
 
-decreaseTime: function() {
-	if(this.seconds > 0) {
-		this.seconds--
-	} else {
-		if(this.minutes > 0) {
-			this.minutes--
-			this.seconds = 59;
+	decreaseTime: function() {
+		if(this.seconds > 0) {
+			this.seconds--
 		} else {
-			this.minutes = -1;
-			this.seconds = -1;	
-		}
-	}	
-},
+			if(this.minutes > 0) {
+				this.minutes--
+				this.seconds = 59;
+			} else {
+				this.minutes = -1;
+				this.seconds = -1;	
+			}
+		}	
+	},
 
-increaseTime: function() {
-	if(this.seconds < 59) {
-		this.seconds++
-	} else {
-		this.seconds = 0;
-		this.minutes++;
-	}
-},
+	increaseTime: function() {
+		if(this.seconds < 59) {
+			this.seconds++
+		} else {
+			this.seconds = 0;
+			this.minutes++;
+		}
+	},
 })
--- node_helper.js
+++ node_helper.js
@@ -7,6 +7,8 @@
 
 var NodeHelper = require("node_helper");
 var fs = require('fs');
+var defaultData = { today: [] };
+var dataPath = 'modules/MMM-Pomodoro/data.json';
 
 module.exports = NodeHelper.create({
 
@@ -20,17 +22,12 @@ module.exports = NodeHelper.create({
 	 */
 	socketNotificationReceived: function(notification, payload) {
 		if (notification === "MMM-Pomodoro-SAVEDATA") {
-      console.log("Dau xanh rau ma");
-      console.log(payload)
+      this.addSuccessPomodoro(payload);
+
 			// console.log("Working notification system. Notification:", notification, "payload: ", payload);
 			// Send notification
-			// this.sendNotificationTest(this.anotherFunction()); //Is possible send objects :)
+      this.sendSocketNotification("MMM-Pomodoro-SAVEDATA", this.returnPayload());
 		}
-	},
-
-	// Example function send notification test
-	sendNotificationTest: function(payload) {
-		this.sendSocketNotification("MMM-CasperValidator-NOTIFICATION_TEST", payload);
 	},
 
 	// this you can create extra routes for your module
@@ -48,27 +45,67 @@ module.exports = NodeHelper.create({
 		return {date: new Date()};
 	},
 
-  // readData: function(){
-  //   let jsonData;
+  readData: function(){
+    var jsonData;
 
-  //   fs.readFile('modules/MMM-Pomodoro/data.json', (err, data) => {
-  //     if (err)
-  //       jsonData = { today: [] };
-  //     else
-  //       jsonData = JSON.parse(data);
-  //   });
+    try {
+      jsonData = JSON.parse(fs.readFileSync(dataPath));
+    } catch (err) {
+      console.log(err);
+      jsonData = defaultData;
+    }
 
-  //   return jsonData;
-  // },
+    return jsonData;
+  },
 
-  // addSuccessPomodoro: function(data){
-  //   let jsonData = this.readData();
-  //   jsonData.today = jsonData.today.filter( e => e.time )
-  //   let data = JSON.stringify(student, null, 2);
+  filterTodayPomodoro: function(array) {
+    let midnightToday = new Date;
+    midnightToday.setHours(0,0,0,0);
+
+    return array.filter( x => Date.parse(x.time) >= midnightToday );
+  },
+
+  addSuccessPomodoro: function(payload){
+    let jsonData = this.readData();
+    jsonData.today = this.filterTodayPomodoro(jsonData.today);
+    jsonData.today.push(payload);
+
+    let data = JSON.stringify(jsonData, defaultData, 2);
     
-  //   fs.writeFile('student-3.json', data, (err) => {
-  //       if (err) throw err;
-  //       console.log('Data written to file');
-  //   });
-  // }
+    fs.writeFileSync(dataPath, data, (err) => {
+      if (err) throw err;
+    });
+  },
+
+  detectNextCycleType: function(){
+    let todayCycles = this.readData().today.reverse();
+
+    if (todayCycles[0].type != "pomodoro") {
+      return "pomodoro";
+    } else {
+      let recentPomodoroCycles = todayCycles.filter( x => x.type == "pomodoro" || x.type == "relax-30" ).slice(0, 4);
+
+      for (var i = 0; i < recentPomodoroCycles.length - 1; i++) {
+        let diffTime = Math.abs(Date.parse(recentPomodoroCycles[i].time) - Date.parse(recentPomodoroCycles[i+1].time));
+        if (diffTime > 15*60*1000) {
+          return "relax-5";
+        }
+      }
+  
+      if (recentPomodoroCycles.filter( x => x.type == "pomodoro").length == 4) {
+        return "relax-30";
+      } else {
+        return "relax-5";
+      }
+    }
+  },
+
+  returnPayload: function() {
+    let data = this.readData();
+
+    return {
+      today_cycle: data.today.length,
+      next_type: this.detectNextCycleType()
+    }
+  }
 });
